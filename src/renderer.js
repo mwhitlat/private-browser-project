161e1216b29f74d9a54f9d3728ef0f15f3c879b4
Matt Whitlatch <mwhitlatch34@gmail.com>
2025-07-16 20:00:53 -0500

    Add zoom level setting to webview

--- src/renderer.js
+++ src/renderer.js
@@ -233,6 +233,7 @@ function setupTabWebviewEvents(tab) {
         console.log(`Tab ${tab.id}: DOM ready`);
         
         // Set zoom factor to 10       tab.webview.setZoomFactor(1.0);
+        tab.webview.setZoomLevel(0);  // 0
         
         // Temporarily disable CSS injection to test basic functionality
         console.log('CSS injection disabled for testing');
